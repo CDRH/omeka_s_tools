ec507cec31cc37418415c3059e3b988a700dbe70
nichgray <ngray2@unl.edu>
2026-06-02 10:25:51 -0500

    use ingester declared in payload

--- omeka_s_tools/api.py
+++ omeka_s_tools/api.py
@@ -309,7 +309,7 @@ class OmekaAPIClient(object):
 
     # ADDING ITEMS
 
-    def prepare_property_value(self, value, property_id):
+    def prepare_property_value(self, value, property_id, label=""):
         '''
         Formats a property value according to its datatype as expected by Omeka.
         The formatted value can be used in a payload to create a new item.
@@ -317,6 +317,7 @@ class OmekaAPIClient(object):
         Parameters:
         * `value` - a dict containing a `value` and (optionally) a `type`
         * `property_id` - the numeric identifier of the property
+        * `label` - display label for URI values
 
         Note that is no `type` is supplied, 'literal' will be used by default.
 
@@ -344,8 +345,10 @@ class OmekaAPIClient(object):
             property_value['@value'] = value['value']
         elif data_type == 'uri':
             property_value['@id'] = value['value']
-            if value['label']:
-                property_value['o:label'] = value['label']
+            if label == "":
+                property_value["o:label"] = value["value"].split("/")[-1]
+            else:
+                property_value["o:label"] = label
         else:
             property_value['@value'] = value['value']
         return property_value
@@ -537,6 +540,9 @@ class OmekaAPIClient(object):
         Returns:
         * a dict providing a JSON-LD representation of the new media object
         '''
+        if payload is None:
+        payload = {}
+
         files = {}
         # For backwards compatibility
         if isinstance(media_file, dict):
@@ -553,8 +559,11 @@ class OmekaAPIClient(object):
                 class_id = template['o:resource_class']['o:id']
         if class_id:
             payload['o:resource_class'] = self.format_resource_id(class_id, 'resource_classes')
+        # Use the ingester declared in the payload, falling back to "upload".
+        # Using .get() guards against a missing key
+        ingester = payload.get("o:ingester") or "upload"
         file_data = {
-            'o:ingester': 'upload',
+            'o:ingester': ingester,
             'file_index': '0',
             'o:source': path.name,
             'o:item': {'o:id': item_id},
